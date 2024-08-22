77e53ef8934c1b95dea869332e02fdd9ee842fcf
Valen <valenzalazar3@gmail.com>
2024-08-22 11:48:55 -0300

    error type

---
+++ src/Ejer05.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ejer05 {
+    public static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+       
+        int numero;
+
+        
+        System.out.println("Ingresa un numero entero: ");
+        numero = scanner.nextInt();
+        
+        
+        
+        boolean esDeUnSoloDigito=  (numero < 10) ? true : false;
+        boolean esImpar= (numero % 2 == 0) ? false :  true;
+        boolean estaEnAmbosGrupos =  (esDeUnSoloDigito && esImpar) ? true : false;
+        boolean noEstaEnNingunGrupo= (!esDeUnSoloDigito || !esImpar) ? true : false;
+
+    
+        System.out.println("¿Es de un solo digito?" + esDeUnSoloDigito);
+        System.out.println("¿Es impar?" + esImpar);
+        System.out.println("¿Esta en ambos grupos?" + estaEnAmbosGrupos);
+        System.out.println("¿No esta en ningun grupo?" + noEstaEnNingunGrupo);
+
+
+    }
+    
+}
