52219a3497b32b1dae4c99ea4646cff795feea35
Valen <valenzalazar3@gmail.com>
2024-08-21 22:21:44 -0300

    EjerTodoCode

---
+++ src/Ejer06.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class Ejer06 {
+
+    public static final Scanner scanner = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+
+       String continuar = "continuar";
+     
+
+
+         while(continuar.equals("continuar")) {
+            int num1;
+            int num2;
+            int result;
+            String simbol;
+            String message =  "Felicidades su resultado es: ";
+
+
+      System.out.println("Ingrese su simbolo de operación(+, -, /, *): ");
+        simbol = scanner.nextLine();
+      System.out.println("Ingrese su primer numero: ");
+        num1 = scanner.nextInt();
+      System.out.println("Ingrese su segundo numero: ");
+      num2 = scanner.nextInt();
+
+      if(simbol.equals("+")){
+        result = num1 + num2;
+        System.out.println(message + result);
+      }
+      if(simbol.equals("-")){
+        result = num1 - num2;
+        System.out.println(message + result);
+      }
+      if(simbol.equals("/")){
+        if (num1 == 0 || num2 == 0) {
+            System.out.println("No se puede poner cero en ningun número!");
+           
+        }else{
+
+            result = num1 / num2;
+            System.out.println(message + result);
+        }
+      }
+      if(simbol.equals("*")){
+        result = num1 + num2;
+        System.out.println(message + result);
+      }
+      
+      scanner.nextLine();
+        
+      
+      System.out.println("Si deseas seguir escribe 'Continuar' o 'Salir' en caso contrario");
+      
+      continuar = scanner.nextLine().toLowerCase();
+      System.out.println(continuar);
+      
+    }
+
+    }
+}
